15a5a0dac938cb7ecbbcd6aedead15146037c123
ArghittuThms <thomas.arghittu@gmail.com>
2022-08-24 11:39:12 +0100

    Data Analysis

--- Data Analysis.R
+++ Data Analysis.R
@@ -30,7 +30,7 @@ library(corrplot)
   ## Analisi caratteristiche di ogni genere
     
     # Creazione data-frame per visualizzazione delle caratteristiche di ogni genere
-    temp <- names(audio_features)[c(5:8, 10:15, 22, 24)]
+    temp <- names(audio_features)[c(5, 6, 8, 10:15)]
     
     genre_analysis <- audio_features %>%
       select(c("genre", temp)) %>%
@@ -41,9 +41,9 @@ library(corrplot)
       ggplot(aes(x = value)) +
       geom_density(aes(color = genre)) +
       facet_wrap(~name, ncol = 3, scales = 'free') +
-      labs(title = 'Songs characteristics',x = '', y = '') +
+      labs(title = 'Genre Characteristic',x = '', y = '') +
       theme_bw() +
-      theme(axis.text.x = element_text(angle = 50, hjust = 1),axis.text.y = element_blank(), aspect.ratio = 0.30)
+      theme(axis.text.x = element_text(angle = 50, hjust = 1),axis.text.y = element_blank())
   
     
   ## Analisi caratteristiche medie (con istogramma) di ogni genere (solo con 3 features)
@@ -93,15 +93,31 @@ library(corrplot)
   
   
 ## ANALISI SPECIFICA CANZONI/GENERI PIU POPOLARI
-  
-  ## Caratteristiche delle canzoni famose
     
-    # Creazione data-frame con le 100 canzoni più popolari
+    # Creazione data-frame con le 200 canzoni più popolari
     top_200_song <- audio_features %>%
       select(popularity, danceability, energy, key, loudness, mode, speechiness,acousticness, 
-             instrumentalness, liveness, valence, tempo, duration_ms) %>%
+             instrumentalness, liveness, valence, tempo, duration_s) %>%
       arrange(desc(popularity)) %>%
-      head(100)
+      head(200)
+    
+    
+  ## Genere delle top 200 canzoni
+    
+    # Grafico
+    top_200_song %>%
+      count(genre) %>%
+      ggplot() +
+      geom_col(aes(x = genre, y = n, fill = genre)) +
+      coord_polar() +
+      theme_bw() +
+      theme(axis.text.x = element_text(hjust = 1), axis.text.y = element_text(hjust = 1)) + 
+      ggtitle("Genre of most popular song") + 
+      xlab("Song Genre") + 
+      ylab("Number of songs")
+      
+    
+  ## Caratteristiche delle canzoni famose
     
     temp3 <- names(top_200_song)[c(3,4,6,8:13)]
     
@@ -109,7 +125,7 @@ library(corrplot)
       select(c("genre", temp3)) %>%
       pivot_longer(cols = temp3)
     
-    
+    # Grafico che mostra le caratteristiche delle canzoni più popolari
     top_200_analysis %>%
       ggplot(aes(x = name, y = value)) +
       geom_jitter(aes(color = genre)) +
@@ -118,7 +134,77 @@ library(corrplot)
       theme_bw()+
       theme(axis.text.y = element_blank(), axis.text.x = element_blank())
   
+    
+  ## Durata delle canzoni più popolari
+    
+    # Grafico
+    ggplot(data = top_200_song, mapping = aes(x = duration_s)) +
+      geom_density(aes(color = genre)) +
+      labs(title = 'Duration in sec of most popularity song',x = '', y = '') +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 50, hjust = 1),axis.text.y = element_blank())
+    
   
+  ## Chiave e modalità delle canzoni più popolari
+    
+    top_200_song <- top_200_song %>%
+      mutate(key_name = map(key, function(x){
+        switch (x,
+          is.na : NULL,
+          is.nan : NULL,
+          -1 : as.character("NS", na.rm = TRUE),
+          1 : as.character("DO", na.rm = TRUE),
+          2 : as.character("Do#", na.rm = TRUE),
+          3 : as.character("Re", na.rm = TRUE),
+          4 : as.character("Re#", na.rm = TRUE),
+          5 : as.character("Mi", na.rm = TRUE),
+          6 : as.character("Fa", na.rm = TRUE),
+          7 : as.character("Fa#", na.rm = TRUE),
+          8 : as.character("Sol", na.rm = TRUE),
+          9 : as.character("Sol#", na.rm = TRUE),
+          10 : as.character("La", na.rm = TRUE),
+          11 : as.character("La#", na.rm = TRUE),
+          12 : as.character("Si", na.rm = TRUE)
+        )
+      }))
+    
+    # Grafico
+    ggplot(data = top_200_song, mapping = aes(x = key, fill = genre)) +
+      geom_bar(aes(color = genre), position="dodge") +
+      labs(title = 'Key of most popularity song',x = '', y = '') +
+      theme_bw() +
+      scale_x_continuous(breaks = 0:11, labels = c("C", "C #", "D", "D #", "E", "F", "F #", "G", "G #", "A", "A #", "B")) +
+      theme(axis.text.x = element_text(angle = 50, hjust = 1), axis.text.y = element_blank())
+    
+    
+    ggplot(data = top_200_song, mapping = aes(x = key)) +
+      geom_density(aes(color = genre)) +
+      labs(title = 'Key of most popularity song',x = '', y = '') +
+      theme_bw() +
+      scale_x_continuous(breaks = 0:11, labels = c("C", "C #", "D", "D #", "E", "F", "F #", "G", "G #", "A", "A #", "B")) +
+      theme(axis.text.x = element_text(angle = 50, hjust = 1), axis.text.y = element_blank())
+    
+    
+  ## Confronto generi delle canzoni più popolari con generi a livello generale
+    
+    # Grafico
+    audio_features %>%
+      count(genre) %>%
+      ggplot() +
+      geom_col(aes(x = genre, y = n, fill = genre)) +
+      coord_polar() +
+      theme_bw() +
+      theme(axis.text.x = element_text(hjust = 1), axis.text.y = element_text(hjust = 1)) + 
+      ggtitle("Genre of most popular song") + 
+      xlab("Song Genre") + 
+      ylab("Number of songs")
+    
+      
+    
+    
+    
+    
+    
   
   
   
